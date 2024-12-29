f0fafc8010b7176a454efb5026d330e4deed4199
RostyslavNikolaiev <r.t.nikolaiev@student.csn.khai.edu>
2024-12-29 08:50:28 +0200

    Додано завдання для Task02

---
+++ Task02/Figura.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_02
+{
+    public partial class RenderControl
+    {
+        public class Figura
+        {
+            readonly double cursorCenterX = -3;
+            readonly double cursorCenterY = 0;
+
+            public void PaintCursor()
+            {
+                glPointSize(5);
+                glColor3d(0, 0, 1);
+                glBegin(GL_POINTS);
+                glVertex2d(cursorCenterX, cursorCenterY);
+
+                glEnd();
+                glDisable(GL_POINTS);
+            }
+
+            public void PaintComplexFigure(int sideSize = 5, uint DrawMode = GL_FILL, double offsetX = 0, double offsetY = 0)
+            {
+
+                glPointSize(5);
+                glPolygonMode(GL_FRONT_AND_BACK, DrawMode);
+                glShadeModel(GL_FLAT);
+
+                glBegin(GL_TRIANGLE_FAN);
+                glColor3d(1, 1, 0);
+                glVertex2d(cursorCenterX + offsetX, cursorCenterY + offsetY);
+                glVertex2d((cursorCenterX + sideSize / 2 + 1) + offsetX, (cursorCenterY + sideSize / 2.5) + offsetY);
+                glVertex2d((cursorCenterX + sideSize / 2 + 1) + offsetX, (cursorCenterY - sideSize / 2.5) + offsetY);
+
+                glEnd();
+
+                glBegin(GL_POLYGON);
+                glColor3d(1, 0, 0);
+                glVertex2d(cursorCenterX + sideSize / 2 + 1 + offsetX, cursorCenterY + sideSize / 2.5 + offsetY);   
+                glVertex2d(cursorCenterX + sideSize + 2 + offsetX, cursorCenterY + sideSize / 2.5 + offsetY);       
+                glVertex2d(cursorCenterX + sideSize + 2 + offsetX, cursorCenterY - sideSize / 2.5 + offsetY);       
+                glVertex2d(cursorCenterX + sideSize / 2 + 1 + offsetX, cursorCenterY - sideSize / 2.5 + offsetY);   
+                glEnd();
+                glBegin(GL_POLYGON);
+                glColor3d(0, 0.5, 0);
+                glVertex2d(cursorCenterX + sideSize / 2 + 1 + offsetX, cursorCenterY - sideSize / 2.5 + offsetY);   
+                glVertex2d(cursorCenterX + sideSize + 2 + offsetX, cursorCenterY - sideSize / 2.5 + offsetY);       
+                glVertex2d(cursorCenterX + sideSize + 2 + offsetX, cursorCenterY - sideSize - 1 + offsetY);        
+                glVertex2d(cursorCenterX + sideSize / 2 + 1 + offsetX, cursorCenterY - sideSize - 1 + offsetY);     
+                glEnd();
+
+                glBegin(GL_TRIANGLE_FAN);
+                glColor3d(1, 1, 0);
+                glVertex2d(cursorCenterX + sideSize + 2 + offsetX, cursorCenterY - sideSize / 2.5 + offsetY); 
+                glColor3d(1, 0, 0);
+                glVertex2d(cursorCenterX + sideSize + 2 + offsetX, cursorCenterY - sideSize - 1 + offsetY); 
+                glVertex2d(cursorCenterX + 2 * sideSize + offsetX, cursorCenterY - sideSize + 1 + offsetY); 
+                glColor3d(1, 1, 0);
+                glVertex2d(cursorCenterX + 2 * sideSize + offsetX, cursorCenterY + offsetY); 
+                glColor3d(0, 0.5, 0);
+                glVertex2d(cursorCenterX + sideSize + 2 + offsetX, cursorCenterY + sideSize / 2.5 + offsetY); 
+                glEnd();
+            }
+
+            public void PaintTile(uint DrawMode, double offsetX, double offsetY)
+            {
+                PaintComplexFigure(5, DrawMode, offsetX, offsetY);
+            }
+        }
+    }
+}
---
+++ Task02/Program.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task_02
+{
+    internal static class Program
+    {
+        static Program() => DesignMode = true;
+        public static bool DesignMode { get; set; }
+        [STAThread]
+        static void Main()
+        {
+            DesignMode = false;
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+    }
+}
